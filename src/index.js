6740b21d85c155dd0251af5b47d96bee33b65e2d
daniemart <github email address>
2019-07-16 14:18:00 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#create-task-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    let priority = e.target[1][0].form[1].value
+    let newTask = e.target[0].value
+    if (newTask !== ""){
+      addToList(newTask, priority)
+      e.target[0].value = ""}
+  })
 });
+
+function addToList(task, priority){
+  let toDo = document.querySelector('#tasks')
+  let newListItem = document.createElement('li')
+  newListItem.innerHTML = task + '<button>x</button>'
+  newListItem.id = task
+  if (priority === "low"){
+    newListItem.className = "low"
+  } else if (priority === "medium"){
+    newListItem.className = "medium"
+  } else {
+    newListItem.className = "high"
+  }
+  toDo.appendChild(newListItem)
+
+  newListItem.addEventListener('click', function(e){
+    document.getElementById(task).remove()
+  })
+}
